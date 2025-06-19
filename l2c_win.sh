1d40dff9c637b392b487f5c52b24c2104016a657
jmxyyy <jmxyyy03@gmail.com>
2025-06-19 12:45:24 +0800

    1

--- l2c_win.sh
+++ l2c_win.sh
@@ -3,3 +3,9 @@ cp -r /mnt/c/Users/cxy/AppData/Roaming/Code/User/settings.json ./vscode/win
 
 # zsh
 cp ~/.zshrc ./zsh/win
+
+# starship
+cp ~/.config/starship.toml .
+
+# nvim
+cp -r ~/.config/nvim/ .
